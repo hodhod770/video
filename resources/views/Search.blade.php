01dc2b2eacbf9f9a13264eb8d93c61267e37675b
hodhod770 <alhodhod.yemen@gmail.com>
2024-07-28 21:05:09 +0300

    Update Search.blade.php

--- resources/views/Search.blade.php
+++ resources/views/Search.blade.php
@@ -5,7 +5,7 @@
 Carbon\Carbon::setLocale('ar');
 @endphp
     <!-- container -->
-    <div dir="rtl" class="">
+    <div dir="rtl" class="m-1">
         @foreach ($results as $item)
         <a href="{{ route('Openv', ['id' => $item->uname]) }}" style="text-decoration: none">
             <div class="card mb-3" >
